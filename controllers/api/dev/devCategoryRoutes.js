74442b90c715a4c83cc3e835339748a5538f9eb9
yacob menghistu <yacobabr@gmail.com>
2023-11-29 21:55:40 -0600

    feature-html-done

--- controllers/api/dev/devCategoryRoutes.js
+++ controllers/api/dev/devCategoryRoutes.js
@@ -1,82 +1,82 @@
-const router = require('express').Router();
-const { User, Category, Product,  Photo } = require('../../../models');
+// const router = require('express').Router();
+// const { User, Category, Product,  Photo } = require('../../../models');
 
-router.get('/', async (req, res) => {
-    // Find all Category records and include other model data
-    try {
-        const data = await Category.findAll({
-            include: [{ model: Product }]
-        });
-        res.status(200).json(data);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-
-router.get('/:id', async (req, res) => {
-    // Find Category record by ID and include other model data
-    try {
-        const data = await Category.findByPk(req.params.id, {
-            include: [{ model: Product }]
-        });
-        // Return an error if record not found
-        if (!data) {
-            res.status(404).json({ message: 'Record ' + req.params.id + ' not found.' });
-            return;
-        }
-        res.status(200).json(data);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// router.get('/', async (req, res) => {
+//     // Find all Category records and include other model data
+//     try {
+//         const data = await Category.findAll({
+//             include: [{ model: Product }]
+//         });
+//         res.status(200).json(data);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 
-router.post('/', async (req, res) => {
-    // create a new category
-    try {
-        const data = await Category.create(req.body);
-        res.status(200).json(data);
-    } catch (err) {
-        res.status(400).json(err);
-    }
-});
+// router.get('/:id', async (req, res) => {
+//     // Find Category record by ID and include other model data
+//     try {
+//         const data = await Category.findByPk(req.params.id, {
+//             include: [{ model: Product }]
+//         });
+//         // Return an error if record not found
+//         if (!data) {
+//             res.status(404).json({ message: 'Record ' + req.params.id + ' not found.' });
+//             return;
+//         }
+//         res.status(200).json(data);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 
-router.put('/:id', async (req, res) => {
-    // update a category by its `id` value
-    try {
-        const data = await Category.update(req.body, {
-            where: { id: req.params.id }
-        });
-        // Return an error if data not found
-        if(data[0] === 0) {
-            res.status(400).json({ message: 'Record ' + req.params.id + ' is not found or updated.' });
-            return;
-        }
-        res.status(200).json({ message: 'Record ' + req.params.id + ' updated.' , updated_to: req.body  });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// router.post('/', async (req, res) => {
+//     // create a new category
+//     try {
+//         const data = await Category.create(req.body);
+//         res.status(200).json(data);
+//     } catch (err) {
+//         res.status(400).json(err);
+//     }
+// });
 
 
-router.delete('/:id', async (req, res) => {
-    // delete a category by its `id` value
-    try {
-        const data = await Category.destroy({
-            where: { id: req.params.id }
-        });
-        if (!data) {
-            res.status(404).json({ message: 'Record ' + req.params.id + ' not found.' });
-            return;
-        }
-        res.status(200).json({ message: 'Record ' + req.params.id + ' is deleted.' });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// router.put('/:id', async (req, res) => {
+//     // update a category by its `id` value
+//     try {
+//         const data = await Category.update(req.body, {
+//             where: { id: req.params.id }
+//         });
+//         // Return an error if data not found
+//         if(data[0] === 0) {
+//             res.status(400).json({ message: 'Record ' + req.params.id + ' is not found or updated.' });
+//             return;
+//         }
+//         res.status(200).json({ message: 'Record ' + req.params.id + ' updated.' , updated_to: req.body  });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
+
+// router.delete('/:id', async (req, res) => {
+//     // delete a category by its `id` value
+//     try {
+//         const data = await Category.destroy({
+//             where: { id: req.params.id }
+//         });
+//         if (!data) {
+//             res.status(404).json({ message: 'Record ' + req.params.id + ' not found.' });
+//             return;
+//         }
+//         res.status(200).json({ message: 'Record ' + req.params.id + ' is deleted.' });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 
 
-module.exports = router;
+// module.exports = router;
--- controllers/api/dev/devPhotoRoutes.js
+++ controllers/api/dev/devPhotoRoutes.js
@@ -1,72 +1,72 @@
-const router = require('express').Router();
-const { User, Category, Product, Photo } = require('../../../models');
-const { sync } = require('../../../models/User');
+// const router = require('express').Router();
+// const { User, Category, Product, Photo } = require('../../../models');
+// const { sync } = require('../../../models/User');
 
-router.get('/', async (req, res) => {
-    try {
-        const data = await Photo.findAll({
+// router.get('/', async (req, res) => {
+//     try {
+//         const data = await Photo.findAll({
 
-        })
-        res.status(200).json(data);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-})
-// route = http://localhost:3001/api/dev/photos/:id
-router.get('/:id', async (req, res) => {
-    try {
-        const data = await Photo.findByPk(req.params.id, {
-        })
-        if (!data) {
-            res.status(404).json({ message: 'Record' + req.params.id + 'not found.' });
-            return;
-        }
-        res.status(200).json(data);
-    } catch (err) {
-        res.status(500).json(err)
-    }
-});
+//         })
+//         res.status(200).json(data);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// })
+// // route = http://localhost:3001/api/dev/photos/:id
+// router.get('/:id', async (req, res) => {
+//     try {
+//         const data = await Photo.findByPk(req.params.id, {
+//         })
+//         if (!data) {
+//             res.status(404).json({ message: 'Record' + req.params.id + 'not found.' });
+//             return;
+//         }
+//         res.status(200).json(data);
+//     } catch (err) {
+//         res.status(500).json(err)
+//     }
+// });
 
-router.post('/', async (req, res) => {
-    try {
-        const data = await Photo.create(req.body);
-        res.status(200).json(data);
-    } catch (err) {
-        res.status(400).json(err);
-    }
-});
+// router.post('/', async (req, res) => {
+//     try {
+//         const data = await Photo.create(req.body);
+//         res.status(200).json(data);
+//     } catch (err) {
+//         res.status(400).json(err);
+//     }
+// });
 
-router.put('/:id', async (req, res) => {
-    try {
-        const data = await Photo.update(req.body, {
-            where: { id: req.params.id }
-        });
+// router.put('/:id', async (req, res) => {
+//     try {
+//         const data = await Photo.update(req.body, {
+//             where: { id: req.params.id }
+//         });
 
-        if (data[0] === 0) {
-            res.status(400).json({ massage: 'Record' + req.params.id + 'is not found or updated' }) // recored of photo?
-            return;
-        }
-        res.status(200).json({ massage: 'Record' + req.params.id + ' update.', updated_to: req.body });
+//         if (data[0] === 0) {
+//             res.status(400).json({ massage: 'Record' + req.params.id + 'is not found or updated' }) // recored of photo?
+//             return;
+//         }
+//         res.status(200).json({ massage: 'Record' + req.params.id + ' update.', updated_to: req.body });
 
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
-router.delete('/:id', async (req, res) => {
-    try {
-        const data = await Photo.destroy({
-            where: { id: req.params.id }
-        });
-        if (!data) {
-            res.status(200).json({ message: 'Record' + req.params.id + 'not found.' });
-            return;
-        }
-        res.status(200).json({ message: 'Record' + req.params.id + 'is deleted.' });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-})
+// router.delete('/:id', async (req, res) => {
+//     try {
+//         const data = await Photo.destroy({
+//             where: { id: req.params.id }
+//         });
+//         if (!data) {
+//             res.status(200).json({ message: 'Record' + req.params.id + 'not found.' });
+//             return;
+//         }
+//         res.status(200).json({ message: 'Record' + req.params.id + 'is deleted.' });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// })
 
 
-module.exports = router; 
+// module.exports = router; 
